89c1094523105d71114a3a7a8f199f8e077b53fc
Hadi Wasim <hadiwasim644@gmail.com>
2024-04-28 07:58:11 -0700

    addition of mouse class

--- Source.cpp
+++ Source.cpp
@@ -1,20 +1,51 @@
 ﻿#include <SFML/Graphics.hpp>
+#include <iostream>
 #include <ctime>
-//#include"../SFML/Images/"
 
 //project setup by Hadi Wasim
 using namespace sf;
 using namespace std;
 
+class MouseEvents : public Mouse {
+	RenderWindow& window;
+	bool mouseHeld = false;
+	int mousePosPressedX = 0;
+	int mousePosPressedY = 0;
+	int mousePosReleasedX = 0;
+	int mousePosReleasedY = 0;
+public:
+	// Constructor
+	MouseEvents(RenderWindow& window) : window(window) {}
 
-struct coordinats {
-	int x;
-	int y;
+	// Handle mouse events
+	void handleEvents() {
+		int mouseX = getPosition(window).x;
+		int mouseY = getPosition(window).y;
+
+		if (isButtonPressed(Left)) {
+			if (!mouseHeld) {  // Check if mouse was previously not held
+				mouseHeld = true;
+				mousePosPressedX = mouseX;
+				mousePosPressedY = mouseY;
+				cout << "Mouse button held at position: " << mousePosPressedX << ", " << mousePosPressedY << endl;
+			}
+		}
+		else {
+			if (mouseHeld) {  // Check if mouse was previously held
+				mouseHeld = false;
+				mousePosReleasedX = mouseX;
+				mousePosReleasedY = mouseY;
+				cout << "Mouse button released at position: " << mousePosReleasedX << ", " << mousePosReleasedY << endl;
+			}
+		}
+	}
+
+};
+class Screen : public Sprite,public MouseEvents {
+
 };
 
-class hello {
-	int games;
-};
+
 
 //Drawing the background
 void createBack(RenderWindow& window) {
@@ -32,13 +63,13 @@ void createBack(RenderWindow& window) {
 //Drawing the map
 void createMap(RenderWindow& window) {
 	//Drawing a map
-	Image map_image;//объект изображения для карты
+	Image map_image;//mage object for map
 	map_image.loadFromFile("Images/grid.png");//load the file for the map
 	Texture map;
 	map.loadFromImage(map_image);
 	Sprite s_map;
 	s_map.setTexture(map);
-	s_map.setPosition(300, 160);
+	s_map.setPosition(360, 160);
 
 	window.draw(s_map);
 }
@@ -74,9 +105,15 @@ int main()
 
 	Clock timeMoney;
 
-
+	
 
 	Clock clock;
+
+
+	//test objects
+	MouseEvents m(window);
+
+
 
 	while (window.isOpen())
 	{
@@ -92,10 +129,10 @@ int main()
 			if (event.type == Event::Closed)
 				window.close();
 		}
-
 		//Create a background
 		createBack(window);
 		createMap(window);
+		m.handleEvents();
 
 
 
